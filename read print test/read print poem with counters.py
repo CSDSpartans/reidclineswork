0edc94124757e37a8e799608f45c5fb8d2c805d0
RedClin <dcline@student.csdspartans.org>
2017-03-21 11:18:02 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -5,38 +5,41 @@ name_of_mydocument = 'tuesdayafternoon.txt'
 file_input = open(name_of_mydocument, 'r')     #open file for reading
 
 line = file_input.readline()
-print(line)
+print('      ', line, end = '')
 line = file_input.readline()
-print(line)
+print('      ', line, end = '')
 line = file_input.readline()
-print(line)
+print(line, end = '')
 line = file_input.readline()
+
 
 line_counter = 1
 stanza_counter = 1
+total_lines_in_file = 3
 
 while line != '':                      # while not end of file
+    total_lines_in_file += 1
     if line == '\n':
       stanza_counter += 1
       print(" ")
+       
     else:
       if line_counter <10:
         print(line_counter, ")   ", line, end = '')
       else:
         print(line_counter, ")  ", line, end = '')
-      line_counter += 1
+      line_counter +=1
       
     line = file_input.readline()
+
  
-total_lines_in_file = (line_counter + 11)
 print(" ")
 print("--End of Poem--")
 print(" ")
 print(" ")
-print("Total number of stanzas in this poem are", stanza_counter)
-print("Total number of lines in this file are", total_lines_in_file)
+print("Total number of stanzas in this poem are", stanza_counter,".")
+print("Total number of lines in this file are", total_lines_in_file,".")
 print(" ")
-print("Tuesday Afternoon first appeared in the album, Days of Future Passed")
-print("Days of Future Passed was released in 1967")
-print("The Moody Blues members were Justin Hayward, Mike Pinder, John Lodge, Graeme Edge, and Ray Thomas")
+print("The song \"Tuesday Afternoon\" first appeared on the album \x1B[3mDays of Future Passed\x1b[23m in 1967.")
+print("The Moody Blues members are Justin Hayward, Mike Pinder,  John Lodge, Graeme Edge, Ray Thomas and the London Festival Orchestra.")
 file_input.close()
